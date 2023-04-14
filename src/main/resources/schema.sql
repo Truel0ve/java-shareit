9fba8235b2a51d01fcb75faa6e01a48144cea5e7
Truelove <pavel.koryakin1990@yandex.ru>
2023-04-15 02:10:55 +0300

    fix: update database 2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,27 +7,27 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS items (
 	item_id			BIGINT			GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	owner_id		BIGINT			REFERENCES users (user_id) ON DELETE CASCADE,
+	owner_id		BIGINT			REFERENCES users (user_id),
 	item_name		VARCHAR(50)		NOT NULL,
 	description		VARCHAR(200)	NOT NULL,
 	available		BOOLEAN			NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS comments (
-	comment_id		BIGINT			GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	content			VARCHAR(200)	NOT NULL,
-	author_id		BIGINT			REFERENCES users (user_id) ON DELETE CASCADE,
-	item_id			BIGINT			REFERENCES items (item_id) ON DELETE cascade,
-	creation_date	TIMESTAMP		DEFAULT CURRENT_TIMESTAMP
+	comment_id		BIGINT							GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	content			VARCHAR(200)					NOT NULL,
+	author_id		BIGINT							REFERENCES users (user_id),
+	item_id			BIGINT							REFERENCES items (item_id),
+	creation_date	TIMESTAMP WITHOUT TIME ZONE
 );
 
 CREATE TYPE booking_status AS ENUM ('WAITING', 'APPROVED', 'REJECTED', 'CANCELED');
 
 CREATE TABLE IF NOT EXISTS bookings (
-	booking_id		BIGINT			GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	start_date		TIMESTAMP		NOT NULL,
-	end_date		TIMESTAMP		NOT NULL,
-	status			booking_status	NOT NULL,
-	booker_id		BIGINT			REFERENCES users (user_id) ON DELETE CASCADE,
-	item_id			BIGINT			REFERENCES items (item_id) ON DELETE CASCADE
+	booking_id		BIGINT							GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	start_date		TIMESTAMP WITHOUT TIME ZONE		NOT NULL,
+	end_date		TIMESTAMP WITHOUT TIME ZONE		NOT NULL,
+	status			booking_status					NOT NULL,
+	booker_id		BIGINT							REFERENCES users (user_id),
+	item_id			BIGINT							REFERENCES items (item_id)
 );
